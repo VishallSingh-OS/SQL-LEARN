ecfd038d4f485f1645c2dce1bfe040094c230585
Vishal Singh <vs6073255@gmail.com>
2025-08-28 21:52:56 +0530

    complete ddl command

--- Begineer_Level/DDLQuery.sql
+++ Begineer_Level/DDLQuery.sql
@@ -10,4 +10,25 @@ CREATE TABLE persons (
 	birth_date DATE,
 	phone VARCHAR(15) NOT NULL,
 	CONSTRAINT pk_persons PRIMARY KEY (id)
-)
+);
+
+SELECT * FROM persons;
+
+-- add a new column email to the persons(table)
+
+ALTER TABLE persons
+ADD email VARCHAR(50) NOT NULL;
+
+SELECT * FROM persons;
+
+
+-- Remove the column phone from the persons table
+
+ALTER TABLE persons
+DROP COLUMN phone
+
+SELECT * FROM persons
+
+-- Delte the table persons from database
+
+DROP TABLE persons
